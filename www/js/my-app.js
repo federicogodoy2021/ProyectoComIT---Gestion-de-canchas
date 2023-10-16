87820fc419163fbe4b192808da85e8f2c8249402
Federico <federicomartingodoy@gmail.com>
2023-10-16 01:05:09 -0300

    Desarrollo de login + registro

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -15,8 +15,28 @@ var app = new Framework7({
     // Add default routes
     routes: [
       {
-        path: '/about/',
-        url: 'about.html',
+      path: '/index/',
+      url: 'index.html',
+      },
+      {
+      path: '/opReg1/',
+      url: 'opReg1.html',
+      },
+      {
+        path: '/opReg2/',
+        url: 'opReg2.html',
+      },
+      {
+        path: '/opReg3/',
+        url: 'opReg3.html',
+      },
+      {
+        path: '/opReg4/',
+        url: 'opReg4.html',
+      },
+      {
+        path: '/opReg5/',
+        url: 'opReg5.html',
       },
     ]
     // ... other parameters
@@ -36,8 +56,22 @@ $$(document).on('page:init', function (e) {
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
+
+})
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="opReg1"]', function (e) {
+    
+})
+$$(document).on('page:init', '.page[data-name="opReg2"]', function (e) {
+    
+})
+$$(document).on('page:init', '.page[data-name="opReg3"]', function (e) {
+
+})
+$$(document).on('page:init', '.page[data-name="opReg4"]', function (e) {
+    
+})
+$$(document).on('page:init', '.page[data-name="opReg5"]', function (e) {
+
 })
